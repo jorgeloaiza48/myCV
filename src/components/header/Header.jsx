05e3b2401805446ce1322f1f52a0e5f5addf9e87
jorgeloaiza48 <jelm48@misena.edu.co>
2022-09-12 18:34:57 -0500

    mejorando CV

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import fotopersonal from "./fotopersonal.jpg"
-import {Smartphone,LocationOn,EmailOutlined} from '@material-ui/icons';
+import {Smartphone,LocationOn,EmailOutlined,ContactMailOutlined,AlternateEmailSharp} from '@material-ui/icons';
 
 
 
@@ -12,16 +12,17 @@ export default function Header() {
           <img src={fotopersonal} alt="foto-personal" className={"headerPhotoImg"}/>
         </div>
         <div className="headerNombres">
-          <h1>JORGE ELIÉCER LOAIZA MUÑOZ hgfhfghfg</h1>
+          <h1>JORGE ELIÉCER LOAIZA MUÑOZ</h1>
           <p>Ingeniero de sistemas y Computación</p>
           <p>Msc. Ingeniería de sistemas y computación</p>
           <p>Desarrollador Web FullStack</p>
         </div>
         <div className="headerContacto">
           <ul className='contactList'>
-              <li className='contactListItem'><p><Smartphone/></p>    <p>3122727084</p></li>           
-              <li className='contactListItem'><p><LocationOn/></p>    <p>Pereira / Risaralda</p></li>
-              <li className='contactListItem'><p><EmailOutlined/></p> <p>jelm48@misena.edu.co</p></li>              
+              <li className='contactListItem'><p><Smartphone/></p>    <p>(+57) 3122727084</p></li>           
+              <li className='contactListItem'><p><LocationOn/></p>    <p>Pereira / Risaralda - Colombia</p></li>
+              <li className='contactListItem'><p><EmailOutlined/></p> <p>jelm48@misena.edu.co</p></li>  
+              <li className='contactListItem'><p><AlternateEmailSharp/></p> <p>jorge.loaiza@utp.edu.co</p></li>            
           </ul>
         </div>       
     </div>
