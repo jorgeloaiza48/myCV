86a26a9900252a1f82cff5ebfc0ebaa58cf9ee76
jorgeloaiza48 <jorge.loaiza@utp.edu.co>
2025-04-29 10:29:03 -0500

    actualizando nodejs

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -38,7 +38,7 @@ function Main() {
       <div className="cvMain">
         <ul>
           <li><p>Ingeniería de Sistemas y Computación 2006. Universidad dfsdfsdsTecnológica de Pereira.</p></li>
-          <li><p>Especialización en Didáctica de la Docencia virtual 2017. Fundación Universitaria del Área Andina. </p></li>
+          <li><p>Especialización en Didáctica de la Docencia virtual 2017. gfgfdgdfFundación Universitaria del Área Andina. </p></li>
           <li><p>Maestría en Ingeniería de Sistemas y Computación2020. Universidad Tecnológica de Pereira.</p></li>
           <li><p>Diplomado en docencia virtual. Politécnico de Colombia 2015.</p></li>
         </ul>
