16aa35d30609cae07c215de091bbd9fd2ab408c5
jorgeloaiza48 <jorge.loaiza@utp.edu.co>
2025-05-01 10:13:14 -0500

    mejornado la HV

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -18,9 +18,10 @@ import oracleCertification from "./oracleCertification.png"
 function Main() {
   return (
     <div className='container-fluid'>
+      <hr></hr>
       <h2 className='mt-5'>Habilidades</h2>
       <div className="cvMain">
-        <div className='d-flex justify-content-around p-3'>
+        <div className='d-flex justify-content-around p-3 flex-wrap'>
           <img src={htmlicon} alt='icon' />
           <img src={cssicon} alt='icon' />
           <img src={jsicon} alt='icon' />
@@ -36,7 +37,7 @@ function Main() {
         </div>
       </div>
 
-
+<hr></hr>
       <h2 className='mt-5'>Formación académica</h2>
       <section class="bsb-timeline-3 bg-light py-3 py-md-5 py-xl-8">
         <div class="">
@@ -102,86 +103,161 @@ function Main() {
           </div>
         </div>
       </section>
+      <hr></hr>
 
       <h2 className='mt-5'>Certificaciones</h2>
       <div className='cvMain'>
-        <img src={oracleCertification} className="mx-auto d-block img-fluid" alt=''/>
+        <img src={oracleCertification} className="mx-auto d-block img-fluid" alt='' />
       </div>
 
 
+<hr></hr>
       <h2 className='mt-5'>Experiencia laboral</h2>
-      
- 
+      <section class="bsb-timeline-1 py-5 py-xl-8">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-10 col-md-8 col-xl-6">
 
-<section class="bsb-timeline-1 py-5 py-xl-8">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-10 col-md-8 col-xl-6">
-
-        <ul class="timeline">
-          <li class="timeline-item">
-            <div class="timeline-body">
-              <div class="timeline-content">
-                <div class="card border-0">
-                  <div class="card-body p-0">
-                    <h5 class="card-subtitle text-secondary mb-1">2007-2008</h5>
-                    <h2 class="card-title mb-3">Secretaría departamental de RISARALDA <br></br>Docente Asesor del área informática</h2>
-                    <p class="card-text m-0">Tecnologías empleadas: C++, VISUALBASIC</p>
+              <ul class="timeline">
+                <li class="timeline-item">
+                  <div class="timeline-body">
+                    <div class="timeline-content">
+                      <div class="card border-0">
+                        <div class="card-body p-0">
+                          <h5 class="card-subtitle text-secondary mb-1">2007-2008</h5>
+                          <h2 class="card-title mb-3">Secretaría departamental de RISARALDA <br></br>Docente Asesor del área informática</h2>
+                          <p class="card-text m-0">Tecnologías empleadas: C++, VISUALBASIC</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
+                </li>
+
+                <li class="timeline-item">
+                  <div class="timeline-body">
+                    <div class="timeline-content">
+                      <div class="card border-0">
+                        <div class="card-body p-0">
+                          <h5 class="card-subtitle text-secondary mb-1">2009-2024</h5>
+                          <h2 class="card-title mb-3">SENA<br></br>Instructor de informática(ANÁLISIS Y DESARROLLO DE SOFTWARE, EXCEL AVANZADO, BD, ESTADÍSTICA, MATEMÁTICA, ÁLGEBRA)</h2>
+                          <p class="card-text m-0">Tecnologías empleadas: PYTHON, VISUALBASIC, SQL, HTML, CSS, JavaScript, NODE JS, REACTJS, TAILWINDCSS, BOOTSTRAP</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li class="timeline-item">
+                  <div class="timeline-body">
+                    <div class="timeline-content">
+                      <div class="card border-0">
+                        <div class="card-body p-0">
+                          <h5 class="card-subtitle text-secondary mb-1">2022</h5>
+                          <h2 class="card-title mb-3">Digital House<br></br>Desarrollador WebFullStack</h2>
+                          <p class="card-text m-0">Tecnologías empleadas: HTML, CSS, JAVASCRIPT, NODEJS, REACT
+                            SEQUELIZE, EXPRESS, SQL. Proyecto desarrollado: https://myecommerce.onrender.com/</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+<hr></hr>
+      <h2 className='mt-4 mb-4'>Proyectos desarrollados</h2>
+      {/* https://bestjquery.com/tutorial/timeline/demo175/ */}
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="main-timeline">
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2020</div>
+                  <h3 class="title">Tienda virtual</h3>
+                  <p class="description">Este proyecto consiste en una tienda virtual para la venta de calzado.<br></br><strong>Tecnologías empleadas: JavaScript, EJS, CSS, HTML</strong></p>
+                  <a href='https://myecommerce.onrender.com/'>https://myecommerce.onrender.com/</a>
                 </div>
               </div>
-            </div>
-          </li>
-
-          <li class="timeline-item">
-            <div class="timeline-body">
-              <div class="timeline-content">
-                <div class="card border-0">
-                  <div class="card-body p-0">
-                    <h5 class="card-subtitle text-secondary mb-1">2009-2024</h5>
-                    <h2 class="card-title mb-3">SENA<br></br>Instructor de informática(ANÁLISIS Y DESARROLLO DE SOFTWARE, EXCEL AVANZADO, BD, ESTADÍSTICA, MATEMÁTICA, ÁLGEBRA)</h2>
-                    <p class="card-text m-0">Tecnologías empleadas: PYTHON, VISUALBASIC, SQL, HTML, CSS, JavaScript, NODE JS, REACTJS, TAILWINDCSS, BOOTSTRAP</p>
-                  </div>
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2021</div>
+                  <h3 class="title">Programador de cursos complementarios.</h3>
+                  <p class="description">
+                    Este proyecto realizado en Excel mediante VBA permite realizar la programación de cursos o llevar un control calendario de los mismos.<br></br><strong>Tecnologías empleadas: VisualBasic for Applications</strong>
+                  </p>
                 </div>
               </div>
-            </div>
-          </li>
-
-          <li class="timeline-item">
-            <div class="timeline-body">
-              <div class="timeline-content">
-                <div class="card border-0">
-                  <div class="card-body p-0">
-                    <h5 class="card-subtitle text-secondary mb-1">2022</h5>
-                    <h2 class="card-title mb-3">Digital House<br></br>Desarrollador WebFullStack</h2>
-                    <p class="card-text m-0">Tecnologías empleadas: HTML, CSS, JAVASCRIPT, NODEJS, REACT
-                    SEQUELIZE, EXPRESS, SQL. Proyecto desarrollado: https://myecommerce.onrender.com/</p>
-                  </div>
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2021</div>
+                  <h3 class="title">Validador de usuarios registrados en senasofiaplus.edu.co (Automatización).</h3>
+                  <p class="description">
+                    Este proyecto consiste en pasar una serie de datos en una hoja de cálculo a una librería denominada Selenium para que esta llene por sí misma los campos de un formulario.<br></br><strong>Tecnologías empleadas: Python</strong>
+                  </p>
                 </div>
               </div>
-            </div>
-          </li>          
-        </ul>
 
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2022</div>
+                  <h3 class="title">Registrador de usuarios en senasofiaplus.edu.co (Automatización).</h3>
+                  <p class="description">
+                    Este proyecto consiste en pasar una serie de datos en una hoja de cálculo a una librería denominada Selenium para que esta llene por
+                    sí misma los campos de un formulario y así registrar a los usuarios. Después del proceso genera un reporte en Excel para ver los resultados del proceso<br></br><strong>Tecnologías empleadas: Python</strong>
+                  </p>
+                </div>
+              </div>
+
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2022</div>
+                  <h3 class="title">Programador de cursos complementarios.</h3>
+                  <p class="description">Este proyecto es similar al desarrollado en excel pero esta vez empleando tecnologías Web. <br></br><strong>Tecnologías empleadas: JavaScript, CSS, HTML, REACTJS</strong>
+                  </p>
+                </div>
+              </div>
+
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2023</div>
+                  <h3 class="title">Entorno prácticas concurso DIAN </h3>
+                  <p class="description">Este proyecto consiste en un simulador de preguntas para practicar para las pruebas escritas de los procesos meritocráticos realizados por la CNSC.<br></br><strong>Tecnologías empleadas: JavaScript, CSS, HTML, REACTJS</strong>
+                  </p>
+                </div>
+              </div>
+
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2024</div>
+                  <h3 class="title">Enron Email Data Set</h3>
+                  <p class="description">Este proyecto consiste en visualizar de forma dinámica cada uno de los correos enviados y recibidos por parte de la empresa Enron.<br></br><strong>Tecnologías empleadas: Golang, HTML</strong>
+                  </p>
+                </div>
+              </div>
+
+              <div class="timeline">
+                <div class="timeline-content">
+                  <div class="timeline-year">2024</div>
+                  <h3 class="title">Tienda virtual para la venta de libros</h3>
+                  <p class="description">Este proyecto consiste en un e-comerce para la comercialización de libros.<br></br><strong>Tecnologías empleadas: REACTJS,HTML,CSS, JavaScript</strong>
+                  </p>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
 
 
-
-
-      <h2>Proyectos desarrollados</h2>
-      <div className="cvMain">
-        <ul>
-          <li><p>Tienda virtual. <a href='https://myecommerce.onrender.com/'>https://myecommerce.onrender.com/</a></p></li>
-          <li><p>Programador de cursos complementarios (VisualBasic).</p></li>
-          <li><p>Validador de usuarios registrados en senasofiaplus.edu.co (Automatización).</p></li>
-          <li><p>Registrador de usuarios en senasofiaplus.edu.co (Automatización).</p></li>
-          <li><p>Programador de cursos complementarios. <a href="https://programador-cursos.onrender.com/#/">https://programador-cursos.onrender.com/#/</a></p></li>
-          <li><p>Entorno prácticas concurso DIAN        <a href='https://simulacrosdian.onrender.com/'>https://simulacrosdian.onrender.com/</a></p></li>
-        </ul>
-      </div>
       <h2>Formación Complementaria</h2>
       <div className="cvMain">
         <h4>SENA</h4>
